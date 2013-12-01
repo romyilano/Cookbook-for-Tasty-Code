1734fcbc56b48012f6d4b6222135461b9a9322fe
Romy Ilano <romy@snowyla.com>
2013-12-01 12:05:37 -0800

    gcd ui related tasks start

---
+++ ConcurrencyRecipes/GCD - UI-Related Tasks/GCD - UI-Related Tasks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GCD - UI-Related Tasks
+//
+//  Created by Romy Ilano on 12/1/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ConcurrencyRecipes/GCD - UI-Related Tasks/GCD - UI-Related TasksTests/GCD___UI_Related_TasksTests.m
@@ -0,0 +1,34 @@
+//
+//  GCD___UI_Related_TasksTests.m
+//  GCD - UI-Related TasksTests
+//
+//  Created by Romy Ilano on 12/1/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GCD___UI_Related_TasksTests : XCTestCase
+
+@end
+
+@implementation GCD___UI_Related_TasksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
