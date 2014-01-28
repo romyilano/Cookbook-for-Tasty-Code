5ecc54748cb1c6fc03dd32b162a1706f37065f94
Romy Ilano <romy.ilano@gmail.com>
2014-01-27 19:16:40 -0800

    set up properties for ekeventstore

--- UserDataRecipes/My Events App/My Events App/ViewController.m
+++ UserDataRecipes/My Events App/My Events App/ViewController.m
@@ -18,6 +18,43 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.eventStore = [[EKEventStore alloc] init];
+    
+    // Ask user's permission for accessing the calendar
+    [self.eventStore requestAccessToEntityType:EKEntityMaskEvent completion:^(BOOL granted, NSError *error) {
+        
+        if (granted)
+        {
+            NSDate *now = [NSDate date];
+            
+            NSCalendar *calendar = [NSCalendar currentCalendar];
+            NSDateComponents *fortyEightHoursFromNowComponents = [[NSDateComponents alloc] init];
+            fortyEightHoursFromNowComponents.day = 2; // 48 hours forward
+            
+            // "use NSCalendar to help create future date. More accurate than the alternate method (but
+            //  not by much) NSDate dateWithTimeIntervalSinceNow:
+            NSDate *fortyEightHoursFromNow = [calendar dateByAddingComponents:fortyEightHoursFromNowComponents
+                                                                       toDate:now
+                                                                      options:0];
+            
+            // Use the predicate to retrieve the actual events from the event store
+            NSPredicate *allEventsWithin48HoursPredicate = [self.eventStore predicateForEventsWithStartDate:now endDate:fortyEightHoursFromNow calendars:nil];
+            
+            NSArray *events = [self.eventStore eventsMatchingPredicate:allEventsWithin48HoursPredicate];
+            
+            for (EKEvent *event in events)
+            {
+                NSLog(@"%@", event.title);
+            }
+            
+        }
+        else
+        {
+            NSLog(@"Access not granted: %@", error);
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning
--- UserDataRecipes/My Events App/README.md
+++ UserDataRecipes/My Events App/README.md
@@ -4,4 +4,6 @@ From iOS 6 Recipes / Apress
 
 This sample completely utilizes the Event Kit framework.
 
+"The implementation here is a simple logging of all calendar events within 48 hours from now."
+
 * "Since this app accesses the device's calendars, you should provide a usage description in the project's Info.plist file" Add the Pirvacy -Calendars Usage Description key to the Information property list 
