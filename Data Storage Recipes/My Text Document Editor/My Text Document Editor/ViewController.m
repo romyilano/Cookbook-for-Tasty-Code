1ef09c1d473e4d9b97c7f4437bc8804de2cb303a
Romy Ilano <romy.ilano@gmail.com>
2014-01-27 21:44:28 -0800

    basic setup of data file app

--- Data Storage Recipes/My Text Document Editor/My Text Document Editor/ViewController.m
+++ Data Storage Recipes/My Text Document Editor/My Text Document Editor/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+-(NSString *)currentContentFilePath;
 @end
 
 @implementation ViewController
@@ -26,12 +26,56 @@
     // Dispose of any resources that can be recreated.
 }
 
+# pragma mark - Custom Methods
+// helper method that transforms the relative filename into an absolute file path within the
+//  Documents directory of the Device
+-(NSString *)currentContentFilePath
+{
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [documentDirectories objectAtIndex:0];
+    
+    return [documentsDirectory stringByAppendingPathComponent:self.filenameTextField.text];
+}
+
+#pragma mark - Action methods
 - (IBAction)saveContent:(id)sender {
+    
+    NSString *filePath = [self currentContentFilePath];
+    NSString *content = self.contentTextView.text;
+    
+    NSError *error = nil;
+    
+    BOOL success = [content writeToFile:filePath
+                             atomically:YES
+                               encoding:NSUnicodeStringEncoding
+                                  error:&error];
+    
+    if (!success)
+    {
+        NSLog(@"Unable to save file: %@\nError: %@", filePath, error);
+    }
 }
 
 - (IBAction)loadContent:(id)sender {
+    
+    NSString *filePath = [self currentContentFilePath];
+    NSError *error = nil;
+    
+    NSString *content = [NSString stringWithContentsOfFile:filePath
+                                                  encoding:NSUnicodeStringEncoding
+                                                     error:&error];
+    
+    if (error)
+    {
+        NSLog(@"Unable to laod file: %@\Error: %@", filePath, error);
+    }
+    
+    self.contentTextView.text = content;
 }
 
 - (IBAction)clearContent:(id)sender {
+    
+    self.contentTextView.text = nil;
+    
 }
 @end
