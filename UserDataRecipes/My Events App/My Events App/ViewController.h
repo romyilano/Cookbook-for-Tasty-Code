ee27ce019fc9fd765ed5597d6fa8b08999aa0c80
Romy Ilano <romy.ilano@gmail.com>
2014-01-27 18:34:59 -0800

    set up properties for ekeventstore

--- UserDataRecipes/My Events App/My Events App/ViewController.h
+++ UserDataRecipes/My Events App/My Events App/ViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <EventKit/EventKit.h>
 
 @interface ViewController : UIViewController
+
+/*
+ 
+ "Whenever you're dealing with the Event Kit Framework, the main element you work with is an EKEventStore. This class allows you to access, delete, and save events in your calendars. An EKEventStore takes a relatively long time to initialize, so you should only do it once and store it in a property.
+ */
+
+@property (strong, nonatomic) EKEventStore *eventStore;
+
 
 @end
