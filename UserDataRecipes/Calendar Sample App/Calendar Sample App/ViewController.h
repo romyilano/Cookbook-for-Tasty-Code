cc752f6a29e12353a7a9501fe29c37c525579701
Romy Ilano <romy.ilano@gmail.com>
2014-01-27 16:59:11 -0800

    set up outlets

--- UserDataRecipes/Calendar Sample App/Calendar Sample App/ViewController.h
+++ UserDataRecipes/Calendar Sample App/Calendar Sample App/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *gMonthTextField;
+@property (weak, nonatomic) IBOutlet UITextField *gDayTextField;
+@property (weak, nonatomic) IBOutlet UITextField *gYearTextField;
+@property (weak, nonatomic) IBOutlet UITextField *hMonthTextField;
+@property (weak, nonatomic) IBOutlet UITextField *hDayTextField;
+@property (weak, nonatomic) IBOutlet UITextField *hYearTextField;
 
+- (IBAction)convertToHebrew:(id)sender;
+- (IBAction)convertToGregorian:(id)sender;
 @end
