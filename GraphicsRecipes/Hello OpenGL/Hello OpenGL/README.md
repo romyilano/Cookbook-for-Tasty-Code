e28cbfed74021269f706b23a8861e3202f615ef7
Romy Ilano <romy.ilano@gmail.com>
2014-01-28 20:30:46 -0800

    openGL

---
+++ GraphicsRecipes/Hello OpenGL/Hello OpenGL/README.md
@@ -0,0 +1,4 @@
+# OpenGL ES 2.0 - RayWenderlich
+
+http://www.raywenderlich.com/3664/opengl-tutorial-for-ios-opengl-es-2-0
+
---
+++ GraphicsRecipes/Hello OpenGL/Hello OpenGL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hello OpenGL
+//
+//  Created by Romy Ilano on 1/28/14.
+//  Copyright (c) 2014 Romy Ilano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
