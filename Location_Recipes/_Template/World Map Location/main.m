b7dec8e2a948349ff861688a6ab954e6f076d361
Romy Ilano <romy.ilano@gmail.com>
2014-02-13 10:42:26 -0800

    map kit - blank map kit project

---
+++ Location_Recipes/_Template/World Map Location/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  World Map Location
+//
+//  Created by Romy Ilano on 2/13/14.
+//  Copyright (c) 2014 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
