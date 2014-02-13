dc15b7169133aab469740baaea0f33f0a4f546a1
Romy Ilano <romy.ilano@gmail.com>
2014-02-13 14:00:10 -0800

    Maps with pins - resolving codesigning

--- Location_Recipes/Maps with Pins/World Map Location/ViewController.m
+++ Location_Recipes/Maps with Pins/World Map Location/ViewController.m
@@ -20,6 +20,20 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.mapView.delegate = self;
+    
+    MKPointAnnotation *annotation1 = [[MKPointAnnotation alloc] init];
+    annotation1.title = @"Miami";
+    annotation1.subtitle = @"Annotation 1";
+    annotation1.coordinate = CLLocationCoordinate2DMake(25.802, -80.132);
+    
+    MKPointAnnotation *annotation2 = [[MKPointAnnotation alloc] init];
+    annotation2.title = @"Denver";
+    annotation2.subtitle = @"Annotation 2";
+    annotation2.coordinate = CLLocationCoordinate2DMake(39.733, -105.018);
+    
+    [self.mapView addAnnotation:annotation1];
+    [self.mapView addAnnotation:annotation2];
+    
 }
 
 - (void)didReceiveMemoryWarning
