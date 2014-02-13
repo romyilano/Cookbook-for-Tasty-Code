b4c42123929d2a229922afb4cc88298fc6618b05
Romy Ilano <romy.ilano@gmail.com>
2014-02-13 13:19:23 -0800

    maps with pins - project creation

---
+++ Location_Recipes/Maps with Pins/World Map Location/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  World Map Location
+//
+//  Created by Romy Ilano on 2/13/14.
+//  Copyright (c) 2014 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Location_Recipes/Maps with Pins/World Map LocationTests/World_Map_LocationTests.m
@@ -0,0 +1,34 @@
+//
+//  World_Map_LocationTests.m
+//  World Map LocationTests
+//
+//  Created by Romy Ilano on 2/13/14.
+//  Copyright (c) 2014 Romy Ilano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface World_Map_LocationTests : XCTestCase
+
+@end
+
+@implementation World_Map_LocationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
