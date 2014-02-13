c4d9650b0d456c8d9c3bce345bfea5f31160ee63
Romy Ilano <romy.ilano@gmail.com>
2014-02-13 10:56:27 -0800

    Map Kit sample 1 finished

--- Location_Recipes/World Map Location/World Map Location/ViewController.m
+++ Location_Recipes/World Map Location/World Map Location/ViewController.m
@@ -18,7 +18,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
     self.mapView.delegate = self;
+    
+    // set up region of the map view
+    CLLocationCoordinate2D baltimoreLocation = CLLocationCoordinate2DMake(39.303, -76.612);
+    self.mapView.region = MKCoordinateRegionMakeWithDistance(baltimoreLocation, 10000, 10000);
+    
+    // optional controls
+    self.mapView.zoomEnabled = YES;
+    self.mapView.scrollEnabled = YES;
+    
+    // Control user location on the map
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        self.mapView.showsUserLocation = YES;
+        // can set up the mode to follow user, keeping location in the center.
+        [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+        
+        /*
+         Note:
+         Just because showUserLocation is set to YES the user's location is not automatically
+         visible on the map. To determine whether the location is visible in the current region,
+         use the property userLocationVisible
+         */
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,5 +50,13 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - MKMap Delegate Methods
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    self.userLocationLabel.text = [NSString stringWithFormat:@"Current location: %.5f, %.5f", userLocation.coordinate.latitude, userLocation.coordinate.longitude];
+    
+}
+
 
 @end
